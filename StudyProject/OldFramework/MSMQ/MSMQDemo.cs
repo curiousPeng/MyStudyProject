fa5c80ed516555892c9135a8d93271f09d110f45
CuriousPeng <CuriousPeng@outlook.com>
2020-08-25 14:01:38 +0800

    hotfix

--- StudyProject/OldFramework/MSMQ/MSMQDemo.cs
+++ StudyProject/OldFramework/MSMQ/MSMQDemo.cs
@@ -11,14 +11,27 @@ namespace OldFramework.MSMQ
     {
         public static void SendMessage(Order data)
         {
-            MessageQueue queue = new MessageQueue(".\\private$\\myQueue");
+            string queuePath = @".\private$\myqueue";//位置
+            //远程无法使用Exists(),Create()
+            //string queuePath = @"FormatName:DIRECT=TCP:192.168.1.1\private$\myqueue";// 使用遠程IP指定訊息佇列位置
+
+            if (!MessageQueue.Exists(queuePath))//是否存在
+            {
+                MessageQueue.Create(queuePath);
+            }
+            MessageQueue queue = new MessageQueue(queuePath);
             queue.Send(data);
 
         }
         public static void ReceiveMessage()
         {
-            MessageQueue queue = new MessageQueue(".\\private$\\myQueue");
-            queue.Formatter = new XmlMessageFormatter(new Type[] {typeof(Order)});
+            string queuePath = @".\private$\myqueue";
+            if (!MessageQueue.Exists(queuePath))//是否存在
+            {
+                return;
+            }
+            MessageQueue queue = new MessageQueue(queuePath);
+            queue.Formatter = new XmlMessageFormatter(new Type[] { typeof(Order) });
             try
             {
                 // Receive and format the message.
@@ -29,12 +42,6 @@ namespace OldFramework.MSMQ
                 Console.WriteLine("收到消息Order ID: " +
                     myOrder.orderId.ToString());
             }
-
-            catch (MessageQueueException)
-            {
-                // Handle Message Queuing exceptions.
-            }
-
             // Handle invalid serialization format.
             catch (InvalidOperationException e)
             {
