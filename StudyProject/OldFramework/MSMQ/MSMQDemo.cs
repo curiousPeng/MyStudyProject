62ff0aa25ff33f09fea4205009f955d9e85c9a5c
CuriousPeng <CuriousPeng@outlook.com>
2020-08-25 11:28:53 +0800

    .net framework  msmq

---
+++ StudyProject/OldFramework/MSMQ/MSMQDemo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace OldFramework.MSMQ
+{
+    public class MSMQDemo
+    {
+        public static void SendMessage(Order data)
+        {
+            MessageQueue queue = new MessageQueue(".\\private$\\myQueue");
+            queue.Send(data);
+
+        }
+        public static void ReceiveMessage()
+        {
+            MessageQueue queue = new MessageQueue(".\\private$\\myQueue");
+            queue.Formatter = new XmlMessageFormatter(new Type[] {typeof(Order)});
+            try
+            {
+                // Receive and format the message.
+                Message myMessage = queue.Receive();
+                Order myOrder = (Order)myMessage.Body;
+
+                // Display message information.
+                Console.WriteLine("收到消息Order ID: " +
+                    myOrder.orderId.ToString());
+            }
+
+            catch (MessageQueueException)
+            {
+                // Handle Message Queuing exceptions.
+            }
+
+            // Handle invalid serialization format.
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // Catch other exceptions as necessary.
+
+            return;
+        }
+    }
+
+    public class Order
+    {
+        public Guid orderId;
+        public DateTime orderTime;
+    };
+}
---
+++ StudyProject/OldFramework/Program.cs
@@ -0,0 +1,40 @@
+﻿using OldFramework.MSMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OldFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Order data = new Order { orderId = Guid.NewGuid(), orderTime = DateTime.Now };
+                    MSMQDemo.SendMessage(data);
+                    Console.WriteLine($"发出消息Order Id:{data.orderId}");
+                    Thread.Sleep(3000);
+                }
+            });
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    MSMQDemo.ReceiveMessage();
+                    Thread.Sleep(3000);
+                }
+            });
+
+
+            Console.Read();
+        }
+    }
+}
