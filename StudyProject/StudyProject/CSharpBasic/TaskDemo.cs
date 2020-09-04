2618808b893fd47441fa5e4bdcf5a8e83061594c
CuriousPeng <CuriousPeng@outlook.com>
2020-09-04 17:19:26 +0800

    lock教程demo

--- StudyProject/StudyProject/CSharpBasic/TaskDemo.cs
+++ StudyProject/StudyProject/CSharpBasic/TaskDemo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StudyProject.CSharpBasic
@@ -62,10 +64,10 @@ namespace StudyProject.CSharpBasic
         }
 
         ///并行任务
-        
+
         public static void ParallelInvoke()
         {
-            Parallel.Invoke(Foo,Bar);
+            Parallel.Invoke(Foo, Bar);
         }
         public static void Foo()
         {
@@ -97,5 +99,194 @@ namespace StudyProject.CSharpBasic
             Task t2 = t1.ContinueWith(DoOnSecond);
             t1.Start();
         }
+
     }
+    #region  争用条件和死锁
+
+    //争用条件例子
+    public class StateObject
+    {
+        private int _state = 5;
+        private object _lock = new object();
+        public void ChangeState(int loop)
+        {
+            lock (_lock)//有此lock语句不会出现争用条件
+            {
+                if (_state == 5)
+                {
+                    _state++;
+                    Trace.Assert(_state == 6, $"Race condition occurred after {loop} loops");
+                }
+                _state = 5;
+            }
+        }
+    }
+    public class SampleTaskRace
+    {
+        public void RaceCondition(object o)
+        {
+            Trace.Assert(o is StateObject, "o must be of typeStateObject");
+            StateObject state = o as StateObject;
+
+            int i = 0;
+            while (true)
+            {
+                lock (state)//lock语句不会让争用条件出现，如要实验争用条件则需要注销改语句。
+                {
+                    state.ChangeState(i++);
+                }
+
+            }
+        }
+        public void RaceConditions()
+        {
+            var state = new StateObject();
+            for (int i = 0; i < 2; i++)
+            {
+                Task.Run(() => new SampleTaskRace().RaceCondition(state));
+            }
+        }
+    }
+
+    //死锁例子
+    public class SampleTaskLock
+    {
+        private StateObject _s1;
+        private StateObject _s2;
+        public SampleTaskLock(StateObject s1, StateObject s2)
+        {
+            _s1 = s1;
+            _s2 = s2;
+        }
+
+        public void Deadlock1()
+        {
+            int i = 0;
+            while (true)
+            {
+                lock (_s1)
+                {
+                    lock (_s2)
+                    {
+                        _s1.ChangeState(i);
+                        _s2.ChangeState(i++);
+                        Console.WriteLine($"still runing,{i}");
+                    }
+                }
+            }
+        }
+        public void Deadlock2()
+        {
+            int i = 0;
+            while (true)
+            {
+                lock (_s1)
+                {
+                    lock (_s2)
+                    {
+                        _s1.ChangeState(i);
+                        _s2.ChangeState(i++);
+                        Console.WriteLine($"still runing,{i}");
+                    }
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region lock语句和线程安全
+    public class SharedState
+    {
+        public int State { get; set; }
+    }
+    public class Job
+    {
+        private SharedState _sharedState;
+        private object _lock = new object();
+        public Job(SharedState sharedState)
+        {
+            _sharedState = sharedState;
+        }
+        /// <summary>
+        /// 创建个对象递增2000次，然后下面开多个线程来一起跑，
+        /// </summary>
+        public void DoTheJob()
+        {
+            for (int i = 0; i < 2000; i++)
+            {
+                _sharedState.State += 1;
+            }
+        }
+        /// <summary>
+        /// 开启20个任务跑同一个对象，本来应该会得到40W这个数字，由于没有同步进行，几乎不会得到正确的输出值
+        /// state1加了lock,会得到一个正确的值
+        /// </summary>
+        public static void RunTask()
+        {
+            int numTasks = 20;
+            var state = new SharedState();
+            var state1 = new SharedState();
+            var tasks = new Task[numTasks];
+            var tasks1 = new Task[numTasks];
+            for (var i = 0; i < numTasks; i++)
+            {
+                tasks[i] = Task.Run(() => { new Job(state).DoTheJob(); });
+                tasks1[i] = Task.Run(() => new Job(state).DoThatJob());
+            }
+            Task.WaitAll(tasks);
+            Task.WaitAll(tasks1);
+            Console.WriteLine($"state summarized {state.State}");
+            Console.WriteLine($"state1 summarized {state1.State}");
+        }
+        /// <summary>
+        /// 加上lock语句，让线程每次只能有一个进入，使之变成同步，这样就会得到正确的值
+        /// </summary>
+        public void DoThatJob()
+        {
+
+            for (int i = 0; i < 2000; i++)
+            {
+                lock (_lock)
+                {
+                    _sharedState.State += 1;
+                }
+            }
+        }
+    }
+
+    //Interlocked 此类同比其他同步技术，会快得多，只适合用于简单的同步问题
+    public class InterlockedDemo
+    {
+        private int _state = 0;
+
+        public int StateSyn
+        {
+            get { return Interlocked.Increment(ref _state); }
+        }
+    }
+
+    //Monitor lock语句会被c#解析器翻译为Monitor类。
+    public class MonitorDemo
+    {
+        private object _lock = new object();
+        public void demo1()
+        {
+            lock (_lock)
+            {
+                ///
+            }
+            //上面方法会被翻译成
+            Monitor.Enter(_lock);
+            try
+            {
+                ///
+            }
+            finally
+            {
+                Monitor.Exit(_lock);
+            }
+        }
+
+    }
+    #endregion
 }
