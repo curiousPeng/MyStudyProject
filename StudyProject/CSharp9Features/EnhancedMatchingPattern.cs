5a297130d88fc01662d63fd030350666e1943f3d
Curious Peng <CuriousPeng@outlook.com>
2021-02-03 15:42:57 +0800

    continue

---
+++ StudyProject/CSharp9Features/EnhancedMatchingPattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp9Features
+{
+    public class EnhancedMatchingPattern
+    {
+        public static void Sample()
+        {
+            var p = new Person() { Age = 10, Name = "张三" };
+            if (p is not null and { Age: > 0 })
+            {
+                Console.WriteLine(p.Age);
+            }
+            if (p.Name is not null or { Length: > 0 })
+            {
+                if (p.Name[0] is (> 'a' and < 'z'))
+                {
+                    Console.WriteLine(p.Name);
+                }
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ StudyProject/CSharp9Features/RecordDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp9Features
+{
+    class RecordDemo
+    {
+        public static void demo()
+        {
+            var p1 = new RecordPerson()
+            {
+                Name = "Tom",
+                Age = 12,
+            };
+            Console.WriteLine(p1);
+
+            var p2 = p1 with { Age = 10 };
+            Console.WriteLine(p2);
+
+            var p3 = new RecordPerson() { Name = "Tom", Age = 12 };
+            Console.WriteLine(p3);
+            Console.WriteLine($"p1 Equals p3 =:{p1 == p3}");
+
+            RecordPersonOther p4 = new("Tom", 12);
+            Console.WriteLine(p4);
+        }
+    }
+
+    record RecordPerson
+    {
+        public string Name { get; init; }
+        public int Age { get; init; }
+    }
+    record RecordPersonOther(string Name,int Age);
+}
